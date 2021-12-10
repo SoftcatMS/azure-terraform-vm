1d58bd0e475c3ab232c9c092c6749e1be645cbe8
Nielsen Pierce <nielsenpi@softcat.com>
2021-12-10 13:07:53 +0000

    Move pipeline testing to sandbox

--- tests/advanced/provider.tf
+++ tests/advanced/provider.tf
@@ -3,24 +3,11 @@ terraform {
 
   # Azurerm Backend https://www.terraform.io/docs/language/settings/backends/azurerm.html#
   backend "azurerm" {
-    resource_group_name  = "tfstate"
-    storage_account_name = "tfstate25210"
+    resource_group_name  = "Terraform-Storage-accountRG"
+    storage_account_name = "softcattfmodulestate"
     container_name       = "tf-modules-azure-vm-advanced"
     key                  = "terraform.tfstate"
   }
-
-  # #AWS Backend https://www.terraform.io/docs/language/settings/backends/s3.html
-  # backend "s3" {
-  #   bucket = "bucket_name"
-  #   key    = "path/to/terraform.tfstate"
-  #   region = "bucket_region"
-  # }
-
-  # #GCP Backend https://www.terraform.io/docs/language/settings/backends/gcs.html
-  # backend "gcs" {
-  #   bucket = <bucket_name"
-  #   prefix = "path/to/terraform.tfstate"
-  # }
 
   required_providers {
     # Uncomment required providers and delete unneeded
@@ -28,20 +15,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=2.46.0"
     }
-
-    # aws = {
-    #   version = ">= 3.20"
-    #   source = "hashicorp/aws"
-    # }
-
-    # google = {
-    #   version = ">= 3"
-    #   source = "hashicorp/google"
-    # }
-    # google-beta = {
-    #   version = ">= 3"
-    #   source = "hashicorp/google-beta"
-    # }
   }
 }
 
@@ -52,13 +25,3 @@ provider "azurerm" {
   features {}
 }
 
-# Configure the AWS Provider
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-# Configure the GCP Provider
-# provider "GCP" {
-#   project = "project_name"
-#   region  = "us-central1"
-# }
